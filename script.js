6a26f04b637e8ad8673f2b8705412f190c3bef49
Veysel Tosun <81585680+veyseltosun@users.noreply.github.com>
2021-07-01 13:43:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//selektors
+const input = document.querySelector("#add");
+const btn = document.querySelector("#btn");
+const list = document.querySelector("#list");
+
+//eventlisteners
+btn.addEventListener("click", btnClick);
+list.addEventListener("click", listClick);
+
+//funtions
+function btnClick() {
+    var txt = input.value;
+    if (txt == "") {
+        alert (" you must write something!") ;
+
+    }else{
+        li = document.createElement("li");
+        li.classList.add("element-group__list__item");
+        li.innerHTML = txt;
+        list.insertBefore(li, list.childNodes[0]);
+        input.value ="";
+    }
+}
+function listClick(e) {
+    if (e.target.tagName == "LI") {
+        e.target.classList.toggle("checked"); 
+    }
+}
